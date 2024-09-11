0905570bb74d7e5604f7d5839cae7252c9fe027e
E1WANC1LA <tumami7654321@gmail.com>
2024-09-11 18:20:38 -0300

    Ultimas Modificaciones

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController,private router: Router) {}
 
   ngOnInit() {
+  }
+
+  async Registrarse() {
+    const NombreInput = document.getElementById('nombre') as HTMLInputElement;
+    const apellidoInput = document.getElementById('apellido') as HTMLInputElement;
+    const NombreUsuarioInput = document.getElementById('NombreUsuario') as HTMLInputElement;
+    const contrasenaInput = document.getElementById('contrasena') as HTMLInputElement;
+    const confirmarContrasenaInput = document.getElementById('confirmarContrasena') as HTMLInputElement;
+    const tipoUsuario = document.getElementById('tipoUsuario') as HTMLInputElement;
+
+    const Nombre = NombreInput.value;
+    const apellido = apellidoInput.value;
+    const NombreUsuario = NombreUsuarioInput.value;
+    const contrasena = contrasenaInput.value;
+    const confirmarContrasena = confirmarContrasenaInput.value;
+    const tipo = tipoUsuario.value;
+    let msg = '';
+    console.log(Nombre, apellido, NombreUsuario, contrasena, confirmarContrasena, tipo);
+
+    if (tipo === undefined) {
+      msg += 'Debes seleccionar un tipo de usuario\n';
+    }
+    if (Nombre === '') {
+      msg += 'Debes ingresar un Nombre\n';
+    }
+    if (apellido === '') {
+      msg += 'Debes ingresar un Apellido\n';
+    }
+    if (NombreUsuario === '') {
+      msg += 'Debes ingresar un Nombre de Usuario\n';
+    }
+    if (contrasena === '') {
+      msg += 'Debes ingresar una contraseña\n';
+    }
+    if (confirmarContrasena === '') {
+      msg += 'Debes confirmar la contraseña\n';
+    }
+    if (contrasena !== confirmarContrasena) {
+      msg += 'Las contraseñas no coinciden\n';
+    }
+
+    if (msg !== '') {
+      let alert = await this.alertController.create({
+        header: 'Error',
+        message: msg,
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    NombreInput.value = '';
+    apellidoInput.value = '';
+    NombreUsuarioInput.value = '';
+    contrasenaInput.value = '';
+    confirmarContrasenaInput.value = '';
+
+
+    let alert = await this.alertController.create({
+      header: 'Registro exitoso',
+      message: `Se ha registrado con exito`,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    
+
+
   }
 
 }
